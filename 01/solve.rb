c03dc0dd936428f83cef2c07c7713451658e0925
Lincoln Lee <git@lincoln.hk>
2015-12-15 21:42:54 +0800

    1B

--- 01/solve.rb
+++ 01/solve.rb
@@ -1,11 +1,17 @@
 #!/usr/bin/env ruby
 
 f = File.read 'input.txt'
-up = down = 0
+level = 0
+counter = 1
 
 f.each_line do |l|
-  up += l.count '('
-  down += l.count ')'
+  l.each_char do |char|
+    if char == '('
+      level += 1
+    else
+      level -= 1
+    end
+    puts counter if level == -1
+    counter += 1
+  end
 end
-
-puts (up - down).to_s

34c2b44ede97c90c5a227eeef4928e8cef371c29
Lincoln Lee <git@lincoln.hk>
2015-12-15 22:31:32 +0800

    3B

--- 03/solve.rb
+++ 03/solve.rb
@@ -2,25 +2,26 @@
 
 f = File.read 'input.txt'
 
-total = 1
-cord = [0, 0]
-
+step = 0
+cord = [[0, 0], [0, 0]]
 visited = Hash.new(0)
 
 f.each_line do |line|
   line.each_char do |char|
     case char
     when '^'
-      cord[1] += 1
+      cord[step % 2][1] += 1
     when 'v'
-      cord[1] -= 1
+      cord[step % 2][1] -= 1
     when '<'
-      cord[0] -= 1
+      cord[step % 2][0] -= 1
     when '>'
-      cord[0] += 1
+      cord[step % 2][0] += 1
     end
-    visited["#{cord[0]} #{cord[1]}"] += 1
+
+    visited["#{cord[step % 2][0]} #{cord[step % 2][1]}"] += 1
+    step += 1
   end
 end
 
-puts visited.keys.count + 1
+puts visited.keys.count

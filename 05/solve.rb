f4fcd1385fa1a849373773e928f949c5274e6185
Lincoln Lee <git@lincoln.hk>
2015-12-18 00:41:06 +0800

    5B

--- 05/solve.rb
+++ 05/solve.rb
@@ -1,15 +1,11 @@
 #!/usr/bin/env ruby
 
 def rule1(s)
-  (s.gsub(/([^aeiou])/,'').length) >= 3
+  (s =~ /(..).*\1/) != nil
 end
 
 def rule2(s)
-  (s =~ /([a-z])\1/) != nil
-end
-
-def rule3(s)
-  (s =~ /(ab|cd|pq|xy)/) == nil
+  (s =~ /([a-z]).\1/) != nil
 end
 
 f = File.read 'input.txt'
@@ -17,7 +13,7 @@ f = File.read 'input.txt'
 good = 0
 
 f.each_line do |line|
-  if rule1(line) && rule2(line) && rule3(line)
+  if rule1(line) && rule2(line)
     good += 1
   end
 end
